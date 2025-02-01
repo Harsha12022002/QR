eba0b8282fe590e6006453510b033bd79b733ff9
Harsha <Harshadevadiga6289@gmail.com>
2025-02-01 18:40:14 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+import QRCodeGenerator from './components/QRCODE';
+function App() {
+  return (
+    <div className="App">
+      <QRCodeGenerator/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/QRCODE.jsx
@@ -0,0 +1,29 @@
+import  { useState } from 'react';
+import { QRCodeCanvas } from 'qrcode.react';
+
+const QRCodeGenerator = () => {
+  const [text, setText] = useState('');
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
+      <h1 className="text-3xl font-bold mb-4">QR Code Generator</h1>
+      <input
+        type="text"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        placeholder="Enter text or URL"
+        className="p-2 border rounded w-80 mb-4"
+      />
+      {text && (
+        <QRCodeCanvas
+          value={text}
+          size={256}
+          bgColor="#FFFFFF"
+          fgColor="#000000"
+          level="H"/>
+      )}
+    </div>
+  );
+};
+
+export default QRCodeGenerator;
